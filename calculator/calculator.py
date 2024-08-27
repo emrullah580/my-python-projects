8e77d357f949be572fd1207c2421ae24850430a7
emrullah580 <ssmnemr@gmail.com>
2024-08-27 17:30:20 +0300

    new commıts

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,26 +1,19 @@
+islem = input("1-Toplama", "2-Çikarma", "3-Çarpma", "4-Bölme")
+sa1 = int(input("Birinci sayiyi giriniz: "))
+sa2 = int(input("İkinci sayiyi giriniz: "))
 
-
-def calculator():
-    print("İşlem seçin ")
-    print("1-Toplama", "2-Çikarma", "3-Çarpma", "4-Bölme")
-    
-    islem = input("Seçim Yapin[1-2-3-4]")
-    sa1 = int(input("Birinci sayiyi giriniz: "))
-    sa2 = int(input("İkinci sayiyi giriniz: "))
-    
+def calculator(sayi1, sayi2, islem):
     if islem=="1":
-        print(sa1 + sa2)
-        
+        return sayi1 + sayi2
     elif islem=="2":
-        print(sa1 - sa2)
-        
+        return sayi1 - sayi2
     elif islem=="3":
-        print(sa1 * sa2)
-        
+        return sayi1 * sayi2
     elif islem=="4":
-        print(sa1 / sa2)
-        
+        if sayi2 == 0:
+            return
+        return sayi1 / sayi2
     else:
-        print("Geçersiz")   
+        return  
         
-calculator()
+print(calculator(sa1, sa2, islem))
