a0e10fac820dee7d512848de31b075134f90a272
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-27 18:30:28 +0300

    template

--- guess-number/guess_number.py
+++ guess-number/guess_number.py
@@ -3,6 +3,10 @@ import random
 
 def guess_the_number():
     number = random.randint(1, 100)
-    pass
+    attempts = 0
+    while True:
+        guess_numbers = int(input("PLease enter an integer"))
+        attempts += 1
+        
 
 guess_the_number()
