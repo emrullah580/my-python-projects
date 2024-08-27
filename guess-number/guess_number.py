1bd20a2fb4617163eb3b6dee54ba1f84f381f458
SimplyProgYT <bcemcankaya@gmail.com>
2024-08-27 19:11:45 +0300

    update guess number

--- guess-number/guess_number.py
+++ guess-number/guess_number.py
@@ -4,7 +4,22 @@ def guess_the_number():
     number = random.randint(1, 100)
     attempts = 0
     while True:
-        guess_numbers = int(input("PLease enter an integer"))
+        guess_numbers = int(input("Please enter an integer: "))
         attempts += 1
+
+        if guess_numbers > number:
+            print("Too high!")
+            
+        elif guess_numbers < number:
+            print("Too low!")
+            
+        elif guess_numbers==number:
+            print("Correct!")
+            print("Congrats!")
+            break 
+        
+        if attempts >= 3:
+            break
+        
         
 guess_the_number()
