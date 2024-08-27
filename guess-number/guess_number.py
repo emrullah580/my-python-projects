e0d579a1bd20f60f83808693bfc9d2323242c791
emrullah580 <ssmnemr@gmail.com>
2024-08-27 18:48:18 +0300

    new commits

--- guess-number/guess_number.py
+++ guess-number/guess_number.py
@@ -4,7 +4,22 @@ def guess_the_number():
     number = random.randint(1, 100)
     attempts = 0
     while True:
-        guess_numbers = int(input("PLease enter an integer"))
+        guess_numbers = int(input("Lütfen Bir Değer Giriniz: "))
         attempts += 1
+        if attempts>3:
+            break
+        
+        elif guess_numbers>number:
+            print("Daha küçük değer gir ")
+            
+        elif guess_numbers<number:
+            print("Daha büyük değer gir ")
+            
+        elif guess_numbers==number:
+            print("D O Ğ R U")
+            print("Tebrikler")
+            break 
+        
+     
         
 guess_the_number()
