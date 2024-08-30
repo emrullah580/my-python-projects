9d5dd8bacf2ca313625294c9df83c9cc48bba3d0
emrullah580 <ssmnemr@gmail.com>
2024-08-30 21:28:23 +0300

    creating menu function

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -1,2 +1,9 @@
-def notebook():
-    pass
+
+def menu():
+    print("1- Not Ekleyin ")
+    print("2- Not Silin ")
+    print("3- Notlariniza bakin ")
+    print("4- Programi Kapatin ")
+    
+    
+    
