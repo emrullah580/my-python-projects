fee47f6017c132361ff0b4b31877bc98881768c0
emrullah580 <ssmnemr@gmail.com>
2024-08-30 22:17:13 +0300

    creating see_notes function

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -1,20 +1,24 @@
-notes=[]
+
 def display_menu():
     print("--Notebook Application--")
     print("1-Add new notes")
     print("2-See notes")
     print("3-Delete notes")
     print("4-Exit")
-    
-def New_Notes():
-    new_notes=input("Add new notes: ")
-    notes.append(new_notes)
-
 
 def add_notes(notes):
     new_note = input("Enter your note here: ")
     notes.append(new_note)
     print("New note added successfully!")
+    
+def see_notes(notes):
+    if notes:
+        for index, note in enumerate(notes, start=1):
+            print(f"{index-{note}}")
+            
+        else:
+            print("Şuana kadar not eklemediniz ")
+        
 
 
 def main():
