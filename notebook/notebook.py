53b3467152a63404fca3a5581295d0932a16e633
emrullah580 <ssmnemr@gmail.com>
2024-08-30 21:45:14 +0300

    adding new notes

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -1,8 +1,12 @@
-
+notes=[]
 def display_menu():
     print("--Notebook Application--")
     print("1-Add new notes")
     print("2-See notes")
     print("3-Delete notes")
     print("4-Exit")
+    
+def New_Notes():
+    new_notes=input("Add new notes: ")
+    notes.append(new_notes)
 
