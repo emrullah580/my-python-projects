be7c84389cee2c3d7afe55a5f4ae79b500ec95f9
Cem <bcemcankaya@gmail.com>
2024-08-30 21:28:55 +0300

    updating menu function

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -1,2 +1,6 @@
-def notebook():
-    pass
+def display_menu():
+    print("--Notebook Application--")
+    print("1-Add new notes")
+    print("2-See notes")
+    print("3-Delete notes")
+    print("4-Exit")
