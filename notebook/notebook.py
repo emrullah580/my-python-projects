8350b2055c7fbe27fd006612eeff8621e27eed8c
Cem <bcemcankaya@gmail.com>
2024-08-31 17:08:43 +0300

    delete notes function

--- notebook/notebook.py
+++ notebook/notebook.py
@@ -19,6 +19,28 @@ def view_notes(notes):
             print(f"{index-{note}}")
     else:
         print("There no any notes, let's add some!")
+        
+        
+def delete_notes(notes_list):
+    if notes_list:
+        try:
+            view_notes(notes_list)
+            deleting_index = int(input("Which note dou you want to delete: ")) - 1
+            if 0 <= deleting_index < len(notes_list):
+                while True:
+                    confirm = input("Are you sure you want to delete this note? (yes/no) ").lower()
+                    if confirm != 'no':
+                        removed_note = notes_list.pop(deleting_index)
+                        print(f"'{removed_note}' deleted.")
+                        break
+                    else:
+                        print("Deletion canceled!")
+                        break
+
+        except ValueError:
+            print("Invalid note number!")
+    else:
+        print("There no any notes, let's add some!")
 
 def main():
     notes = []
